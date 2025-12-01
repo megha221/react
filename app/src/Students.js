3de580fab5705cfe637aaa99d2ce5fa7a78cc85a
megha221 <meghabasavaraj9@gmail.com>
2025-12-01 06:26:50 +0000

    added form and usestate to handle thing

--- app/src/Students.js
+++ app/src/Students.js
@@ -1,9 +1,26 @@
+import { useState } from "react";
 function Students(){
-    const students=[
+    const [students,setStudents]=useState([
         {id:1,name:"Meghana",role:"Developer"},
         {id:2,name:"neha",role:"tester"},
         {id:3,name:'ravi',role:"manager"}
-    ]
+    ])
+    const [name,setName]= useState("");
+    const [role,setRole]= useState("");
+ const addStudent=(e)=>{
+  e.preventDefault();
+
+
+  const newStudent={
+    id:students.length+1,
+    name:name,
+    role:role
+  }
+  setStudents([...students,newStudent])
+  setName("");
+  setRole("");
+ }
+
     return(
         <>
         <h1>students</h1>
@@ -15,6 +32,15 @@ function Students(){
             <h1 key={student.id}>{student.name}- {student.role}</h1>
         )
       })}
+
+      <div>
+        <h1>Add new student</h1>
+        <form onSubmit={addStudent}>
+          <input type="name" placeholder="name" value={name} onChange={(e)=>setName(e.target.value)}/>
+          <input type="role" placeholder="role" value={role} onChange={(e)=>setRole(e.target.value)}/>
+          <button type="submit">Add student details</button>
+        </form>
+        </div>
         </>
     )
 }
